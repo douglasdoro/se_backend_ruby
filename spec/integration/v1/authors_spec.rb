2f22644e407d1232a918be62ae0685dace55cf9c
Douglas <douglasdoro@gmail.com>
2024-10-05 15:32:27 -0300

    test: Author's integration test

---
+++ spec/integration/v1/authors_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+RSpec.describe '/v1/authors' do
+  path '/v1/authors' do
+    get('list Authors') do
+      tags 'Authors'
+      produces 'application/json'
+
+      response(200, 'Authors found') do
+        schema type: :object, properties: {
+          authors: {
+            type: :array,
+            items: {
+              type: :object,
+              properties: {
+                id: { type: :string, format: :uuid },
+                name: { type: :string },
+                email: { type: :string }
+              },
+              required: %w[id name email]
+            }
+          },
+          meta: {
+            type: :object,
+            properties: {
+              count: { type: :integer }
+            }
+          }
+        }
+
+        let(:authors) { create_list(:author, 3) }
+
+        run_test!
+      end
+
+      response(200, 'no authors found') do
+        schema type: :object, properties: {
+          authors: { type: :array, items: { type: :object } },
+          meta: {
+            type: :object,
+            properties: {
+              count: { type: :integer }
+            }
+          }
+        }
+
+        let(:authors) { [] }
+        run_test!
+      end
+    end
+  end
+end
