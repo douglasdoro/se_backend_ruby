7ba8c790c10ab56b6b95715e674364f5f8735488
Douglas <douglasdoro@gmail.com>
2024-10-06 09:10:48 -0300

    test: couver Publication CRUD requests

---
+++ spec/requests/v1/publications_spec.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'V1::PublicationsController' do
+  let(:author) { create(:author) }
+  let!(:publication) { create(:publication, author:, status: 'published') }
+  let(:publication_params) { { publication: { title: 'New Title', body: 'New Body', author_id: author.id } } }
+
+  describe 'GET /v1/publications' do
+    it 'returns a list of published publications' do
+      get v1_publications_path
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body[:publications]).not_to be_empty
+    end
+  end
+
+  describe 'GET /v1/publications/:id' do
+    context 'when the publication exists' do
+      it 'returns the publication' do
+        get v1_publication_path(publication.id)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body[:publication][:id]).to eq(publication.id)
+      end
+    end
+
+    context 'when the publication does not exist' do
+      it 'returns a 404 status' do
+        get v1_publication_path(-1)
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body[:error][:message]).to eq('Not found')
+      end
+    end
+  end
+
+  describe 'POST /v1/publications' do
+    context 'with valid parameters' do
+      it 'creates a new publication' do
+        expect do
+          post v1_publications_path, params: publication_params
+        end.to change(Publication, :count).by(1)
+
+        expect(response).to have_http_status(:created)
+        expect(response.parsed_body[:publication][:title]).to eq('New Title')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'returns a 422 status' do
+        invalid_params = { publication: { title: '', body: '', author_id: author.id } }
+        post v1_publications_path, params: invalid_params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.parsed_body).to include(:title, :body)
+      end
+    end
+  end
+
+  describe 'PUT /v1/publications/:id' do
+    context 'with valid parameters' do
+      it 'updates the publication' do
+        patch v1_publication_path(publication.id), params: publication_params
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body[:publication][:title]).to eq('New Title')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'returns a 422 status' do
+        invalid_params = { publication: { title: '', body: '', author_id: author.id } }
+        patch v1_publication_path(publication.id), params: invalid_params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.parsed_body[:error][:message]).to eq('not updated')
+      end
+    end
+  end
+
+  describe 'DELETE /v1/publications/:id' do
+    it 'soft deletes the publication' do
+      delete v1_publication_path(publication.id)
+
+      expect(response).to have_http_status(:no_content)
+      publication.reload
+      expect(publication.status).to eq('deleted')
+      expect(publication.published_at).to be_nil
+      expect(publication.deleted_at).not_to be_nil
+    end
+  end
+end
